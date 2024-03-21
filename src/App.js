b6d33d7cd91010b87bb7c6fb9f96c5f2ce0dd5d6
SamSumanth6 <sumanthvenkatesh6@gmail.com>
2024-03-21 19:15:16 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
 import InputForm from './components/inputForm';
 import Card from './components/card';
-import { useState } from 'react';
+import { useState} from 'react';
 import TimerSound from './assets/timer.mp3';
 
 function App() {
-  const [remainingTime, setRemainingTime] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 })
+  const [remainingTime, setRemainingTime] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
-  const playTimerSound = ()=>{
+  const playTimerSound = () => {
     const audio = new Audio(TimerSound);
     audio.play();
-  }
+  };
+
   return (
     <div className='App'>
       <h1 className='heading'>CountDown <span className='heading-timer'>Timer</span></h1>
-      <InputForm setRemainingTime={setRemainingTime} playTimerSound={playTimerSound} />
+      <InputForm
+        setRemainingTime={setRemainingTime}
+        playTimerSound={playTimerSound}
+      />
       <Card remainingTime={remainingTime} />
     </div>
   );
